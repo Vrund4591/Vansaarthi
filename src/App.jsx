1675ddd6ce41caf8132f1eba6fcbc8ea59060bef
Vrund Patel <vrundsmac@Vrunds-MacBook-Air.local>
2025-05-20 15:47:45 +0530

    Chatbot Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import HomePage from './Components/HomePage'
 import Navigation from './Components/Navigation'
 import { AuthProvider } from './context/AuthContext'
 import AuthSuccess from './Components/AuthSuccess'
+import ChatBot from './Components/ChatBot'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import About from './Components/About'
@@ -12,14 +13,19 @@ function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<HomePage />}/>
-          <Route path="/about" element={<About />}/>
-          <Route path="/projects" element={<Projects />}/>
-          <Route path="/volunteer" element={<Volunteer />}/>
-          <Route path="/auth-success" element={<AuthSuccess />} />
-        </Routes>
+        <div className="min-h-screen flex flex-col justify-between">
+          <Navigation />
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<HomePage />}/>
+              <Route path="/about" element={<About />}/>
+              <Route path="/projects" element={<Projects />}/>
+              <Route path="/volunteer" element={<Volunteer />}/>
+              <Route path="/auth-success" element={<AuthSuccess />} />
+            </Routes>
+          </div>
+          <ChatBot />
+        </div>
       </BrowserRouter>
     </AuthProvider>
   )
